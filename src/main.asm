d5648ed38cf0390d299ae1d1fb1ab600659e405c
Zephryon-zv <ddas38192@gmail.com>
2024-06-07 07:24:35 +0530

    Initial Commit

---
+++ src/main.asm
@@ -0,0 +1,62 @@
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0xBA
+
+start:
+	jmp main
+
+	
+;
+; prints a string to the screen
+; Params: 
+;	- ds:si points to string
+puts:
+	; save registers we'll modify
+	push si
+	push ax
+	push bx
+
+.loop:
+	lodsb				; loads next character in al
+	or al, al			; verify if next character is null
+	jz .done
+
+	mov ah, 0x0e		; call bios interrupt
+	mov bh, 0 			; set page number to 0
+	int 0x10
+
+	jmp .loop
+	
+.done:
+	pop bx
+	pop ax
+	pop si
+	ret
+	 
+ 
+main:
+	; setup data segments
+	mov ax, 0 			; can't write to ds/es directly
+	mov ds, ax
+	mov es, ax
+
+	; setup stack
+	mov ss, ax
+	mov sp, 0x7C00		; stack grows downwards from where we loaded in memory
+
+	; prints message
+	mov si, msg_hello
+	call puts
+
+	hlt
+
+.halt:
+	jmp .halt
+
+
+msg_hello: db 'Hello World by Mrinmoy!', ENDL,  0
+
+times 510-($-$$) db 0
+dw 0AA55h
